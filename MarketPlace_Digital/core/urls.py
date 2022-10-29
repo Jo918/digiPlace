1cb56194784427a85c7fbbe209484c1f92b446a1
Esteban <114707166+Natalia-Parra@users.noreply.github.com>
2022-10-29 09:55:46 -0500

    nuevo commit

--- MarketPlace_Digital/core/urls.py
+++ MarketPlace_Digital/core/urls.py
@@ -7,12 +7,17 @@ from django.urls.conf import include
 from .views import HomeView, UserProductListView, ProductUpdate, ProductDetailView
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+
     path('user/', include('accounts.urls', namespace='users')),
+
+    path('', HomeView.as_view(), name="home" ),
     path('products/', UserProductListView.as_view(), name="product-list"),
     path('products/<slug>/', ProductDetailView.as_view(), name="product-detail"),
     path('products/<slug>/update', ProductUpdate.as_view(), name="product-update"),
+
     path('marketplace/', include('marketplace.urls', namespace="marketplace"))
-    path('market/', include('marketplace', namespace="marketplace"))
 ]
 
 if settings.DEBUG:

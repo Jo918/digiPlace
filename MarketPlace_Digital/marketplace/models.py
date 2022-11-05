1dea365bda4a17a6c61162df9e43d496467c075d
Esteban <114707166+Natalia-Parra@users.noreply.github.com>
2022-11-05 12:11:36 -0500

    commit 2022-11-05

--- MarketPlace_Digital/marketplace/models.py
+++ MarketPlace_Digital/marketplace/models.py
@@ -47,5 +47,13 @@ class PurchasedProduct(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     date_purchased = models.DateTimeField(auto_now_add=True)
 
+    user = models.ForeignKey(
+            User, on_delete=models.CASCADE, related_name="products")
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    thumbnail = models.ImageField(
+        blank=True, null=True, upload_to=marketplace_directory_path)
+    slug = models.SlugField(unique=True)
+
     def __str__(self):
         return self.email
